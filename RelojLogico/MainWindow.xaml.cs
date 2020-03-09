f9c3b6cbbdce0125ec64acce28cc22d3e7cc7d51
bet160 <36347319+bet160@users.noreply.github.com>
2020-03-09 10:11:37 -0600

    Archivos del proyecto
    Código del proyecto creado en visual studio.

---
+++ RelojLogico/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RelojLogico
+{
+    public partial class MainWindow : Window
+    {
+        private List<Proceso> procesos = new List<Proceso>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CrearProcesos();
+        }
+
+        private void CrearProcesos() {
+            Proceso proceso;
+
+            for (int i = 0; i < 4; i++)
+            {
+                proceso = new Proceso();
+                procesos.Add(proceso);
+            }
+        }
+
+        private void EnviarMensaje(int emisor, int receptor, string mensaje, string mensajeRecepcion) 
+        {
+            procesos[emisor].Contador += 1;
+            procesos[emisor].Mensaje = mensaje;
+            RecibirMensaje(emisor, receptor, mensajeRecepcion);
+        }
+
+        private void RecibirMensaje(int emisor, int receptor, string mensaje)
+        {
+            if(procesos[emisor].Contador > procesos[receptor].Contador)
+            {
+                procesos[receptor].Contador = procesos[emisor].Contador + 1;
+            }
+            else
+            {
+                procesos[receptor].Contador += 1;
+            }
+
+            procesos[receptor].Mensaje = mensaje;
+        }
+
+        private void MostrarHistorialProcesoUno()
+        {
+            DG1.Items.Add(procesos[0]);  
+        }
+
+        private void MostrarHistorialProcesoDos()
+        {
+            DG2.Items.Add(procesos[1]);
+        }
+
+        private void MostrarHistorialProcesoTres()
+        {
+            DG3.Items.Add(procesos[2]);
+        }
+
+        private void MostrarHistorialProcesoCuatro()
+        {
+            DG4.Items.Add(procesos[3]);
+        }
+
+        private void ActualizarTabla(int numeroProcesoEmisor, int numeroProcesoReceptor) 
+        {
+            if(numeroProcesoEmisor == 0 || numeroProcesoReceptor == 0)
+            {
+                MostrarHistorialProcesoUno();
+            }
+            if (numeroProcesoEmisor == 1 || numeroProcesoReceptor == 1)
+            {
+                MostrarHistorialProcesoDos();
+            }
+            if (numeroProcesoEmisor == 2 || numeroProcesoReceptor == 2)
+            {
+                MostrarHistorialProcesoTres();
+            }
+            if (numeroProcesoEmisor == 3 || numeroProcesoReceptor == 3)
+            {
+                MostrarHistorialProcesoCuatro();
+            }
+        }
+
+        public void LimpiarTexBox()
+        {
+            TBEmisor.Clear();
+            TBReceptor.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int emisor = int.Parse(TBEmisor.Text) - 1;
+            int receptor = int.Parse(TBReceptor.Text) - 1;
+            string mensajEnvio = "Mensaje enviado";
+            string mensajeRecepcion = "Mensaje recibido";
+
+            EnviarMensaje(emisor, receptor, mensajEnvio, mensajeRecepcion);
+            ActualizarTabla(emisor, receptor);
+            LimpiarTexBox();
+        }
+    }
+}
---
+++ RelojLogico/Proceso.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelojLogico
+{
+    class Proceso
+    {
+        private int id;
+        private int contador;
+        private string mensaje;
+
+        public Proceso()
+        {
+            this.contador = 0;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Contador { get => contador; set => contador = value; }
+        public string Mensaje { get => mensaje; set => mensaje = value; }
+    }
+}
